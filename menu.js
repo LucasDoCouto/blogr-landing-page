ce79149252c529c0d35ffe6f55c437098b0bcfab
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-04 21:52:13 -0400

    mobile menu

--- menu.js
+++ menu.js
@@ -1,9 +1,7 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
+const menuButton = document.querySelector(".hamburguer-menu")
+
+menuButton.addEventListener("click",function(){
+  const isExpanded = JSON.parse(menuButton.getAttribute("aria-expanded"));
+  console.log(isExpanded);
+  menuButton.setAttribute("aria-expanded", !isExpanded)
+})
